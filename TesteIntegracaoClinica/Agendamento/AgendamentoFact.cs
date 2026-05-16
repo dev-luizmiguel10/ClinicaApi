5fd12ec7eb49e5a94cd0f9eabb1f810e3016bcc1
Luiz Aguiar <mluiz8650@gmail.com>
2026-05-15 23:38:14 -0300

    feat: add integration tests for ClinicaApi

---
+++ TesteIntegracaoClinica/Agendamento/AgendamentoFact.cs
@@ -0,0 +1,63 @@
+﻿using ApiClinica.Application.DTO;
+using Azure;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace TesteIntegracaoClinica.Agendamento
+{
+    public class AgendamentoFact:IClassFixture<AgendamentoFactory>
+    {
+        private readonly HttpClient _http;
+        public AgendamentoFact(AgendamentoFactory agendamento)
+        {
+            _http = agendamento.CreateClient();
+        }
+
+        [Fact]
+        public async Task Sucesso()
+        {
+            var agendamento = new AgendamentoDto
+            {
+                MedicoId = 1,
+                PacienteId = 1,
+                data_agendamento = DateTime.Now,
+
+            };
+            var request = await _http.PostAsJsonAsync("/api/Agendamento", agendamento);
+
+            Assert.Equal(HttpStatusCode.Created, request.StatusCode);
+        }
+        [Fact]
+        public async Task Medico_Inexistente()
+        {
+            var medico = new AgendamentoDto { MedicoId = 0 };
+
+            var request = await _http.PostAsJsonAsync("/api/Agendamento", medico);
+            Assert.Equal(HttpStatusCode.BadRequest, request.StatusCode);
+        }
+
+        [Fact]
+        public async Task Paciente_Inexistente()
+        {
+            var paciente = new AgendamentoDto { PacienteId = 0 };
+
+            var request = await _http.PostAsJsonAsync("/api/Agendamento", paciente);
+            Assert.Equal(HttpStatusCode.BadRequest, request.StatusCode);
+        }
+        [Fact]
+        public async Task Data_Medico_Marcada()
+        {
+            var data_medico = new AgendamentoDto
+            {
+                MedicoId = 1,
+                data_agendamento = DateTime.Now
+            };
+
+            var request = await _http.PostAsJsonAsync("/api/Agendamento", data_medico);
+            Assert.Equal(HttpStatusCode.BadRequest, request.StatusCode);
+        }
+    }
+}
---
+++ TesteIntegracaoClinica/Agendamento/AgendamentoFactory.cs
@@ -0,0 +1,50 @@
+﻿using ApiClinica.Domain.Entities;
+using ApiClininca.Infraestrutura.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteIntegracaoClinica.Agendamento
+{
+    public class AgendamentoFactory:WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(s =>
+            {
+                var description= s.Where(d=>d.ServiceType==typeof(DbContextOptions<DbContexto>)
+                 ||
+                d.ServiceType==typeof(DbContextOptions)).ToList();
+
+                foreach (var item in description)
+                {
+                    s.Remove(item);
+                }
+
+                var sql_provider = s.Where(s => s.ServiceType == typeof(IDbContextOptionsConfiguration<DbContexto>)).ToList();
+
+                foreach (var item in sql_provider)
+                {
+                    s.Remove(item);
+                }
+
+                s.AddDbContext<DbContexto>(u => u.UseInMemoryDatabase("db_Api"));
+
+                var sp = s.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<DbContexto>();
+
+                db.Medicos.Add(new Medico { MedicoId = 1, Nome = "Dr. Teste",CRM="3424443244",Especialidade="Odonto" });
+                db.Paciente.Add(new Paciente { PacienteId = 1, Nome = "Paciente Teste",CEP="03474055",CPF="50785077871" });
+                db.SaveChanges();
+            });
+
+        }
+    }
+}
